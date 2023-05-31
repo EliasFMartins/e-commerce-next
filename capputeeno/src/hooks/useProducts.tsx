56465f651d70eea046545a147b0bd654ab298f2c
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-05-31 14:53:36 -0300

    Feat: filtragem de products

--- capputeeno/src/hooks/useProducts.tsx
+++ capputeeno/src/hooks/useProducts.tsx
@@ -1,28 +1,51 @@
 import { ProductsFecthResponse } from "@/types/productsResponse";
 import { useQuery } from "@tanstack/react-query";
 import axios, { AxiosPromise } from "axios";
+import { useFilter } from "./useFilter";
+import { FilterType } from "@/types/filter-types";
+import { PriorityTypes } from "@/types/priority-types";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
-const fetcher = ():AxiosPromise<ProductsFecthResponse> => {
+
+function getCategoryByType (type: FilterType) {
+  if(type === FilterType.MUG) return "mugs";
+  if(type === FilterType.SHIRT) return "t-shirt";
+
+  return'';
+}
+const fetcher = (query:string) : AxiosPromise<ProductsFecthResponse> => {
   return axios.post(
-  API_URL,{
-    query: `
-      query {
-        allProducts {
-          id
-          name
-          price_in_cents
-          image_url
-        }
+  API_URL,
+  { query });
+}
+
+const mountQuery = (type: FilterType, priority: PriorityTypes) => {
+  if(type == FilterType.ALL && priority ===PriorityTypes.POPULARITY) return `query {
+    allProducts(sortField: "sales", sortOrder: "DSC") {
+      id
+      name
+      price_in_cents
+      image_url
+    }
+   }`
+   return `
+    query {
+      allProducts (filter: {category:"${getCategoryByType(type)}"}) {
+        id
+        name
+        price_in_cents
+        image_url
+        category
       }
-    `
-  }
-  );
+    }
+   `
 }
 export function useProducts(){
+  const {type, priority} = useFilter()
+  const query = mountQuery(type,priority)
   const {data} = useQuery({
-    queryFn: fetcher,
-    queryKey: ['products']
+    queryFn:()=> fetcher(query),
+    queryKey: ['products', type]
   })
   return {
     data: data?.data?.data?.allProducts
