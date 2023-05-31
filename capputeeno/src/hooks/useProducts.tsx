912e2cf560d279fa94539e1d208582562afeea89
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-05-31 15:17:49 -0300

    feat: filtragens

--- capputeeno/src/hooks/useProducts.tsx
+++ capputeeno/src/hooks/useProducts.tsx
@@ -13,6 +13,12 @@ function getCategoryByType (type: FilterType) {
 
   return'';
 }
+function getFieldByPriority(priority: PriorityTypes){
+  if(priority === PriorityTypes.NEWS) return {field:"created_at", order: "ASC"}
+  if(priority === PriorityTypes.BIGGEST_PRICE) return {field:"price_in_cents", order: "ASC"}
+  if(priority === PriorityTypes.MINOR_PRICE) return {field:"price_in_cents", order: "DSC"}
+  return {field:"sales", order: "DSC"}
+}
 const fetcher = (query:string) : AxiosPromise<ProductsFecthResponse> => {
   return axios.post(
   API_URL,
@@ -28,9 +34,11 @@ const mountQuery = (type: FilterType, priority: PriorityTypes) => {
       image_url
     }
    }`
+   const sortSettings = getFieldByPriority(priority)
+   const categoryFilter = getCategoryByType(type)
    return `
     query {
-      allProducts (filter: {category:"${getCategoryByType(type)}"}) {
+      allProducts ( sortField: "${sortSettings.field}",sortOrder: "${sortSettings.order}",${categoryFilter? `filter: {category:"${categoryFilter}"}`: ''}) {
         id
         name
         price_in_cents
@@ -45,7 +53,7 @@ export function useProducts(){
   const query = mountQuery(type,priority)
   const {data} = useQuery({
     queryFn:()=> fetcher(query),
-    queryKey: ['products', type]
+    queryKey: ['products', type, priority]
   })
   return {
     data: data?.data?.data?.allProducts
